66db7291a145131c8800935b2734e23d9e7cb471
michaelcodes1993 <lpskirito7@outlook.com>
2020-09-10 19:42:32 -0400

    eddited with prettier

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,56 +1,56 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
-import { Avatar } from "antd";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Avatar } from 'antd';
 const navLinks = [
-	{
-		title: "Home",
-		path: "/",
-	},
+  {
+    title: 'Home',
+    path: '/',
+  },
 
-	{
-		title: "Registration",
-		path: "/registration",
-	},
-	{
-		title: "Login",
-		path: "/login",
-	},
+  {
+    title: 'Registration',
+    path: '/registration',
+  },
+  {
+    title: 'Login',
+    path: '/login',
+  },
 ];
 
 export default function Navigation(props) {
-	const [menuActive, setMenuActive] = useState(false);
-	return (
-		<nav className="site-nav">
-			<span className="menu-title">My Blog</span>
-			<div className={`menu-content-container ${menuActive && "active"}`}>
-				<ul>
-					{navLinks.map((link, index) => (
-						<li key={index}>
-							<NavLink
-								onClick={() => setMenuActive(!menuActive)}
-								exact
-								activeClassName="navsbar"
-								to={link.path}
-							>
-								{link.title}
-							</NavLink>
-						</li>
-					))}
-				</ul>
-				<span className="menu-avatar-container">
-					<Avatar
-						src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-						size="38"
-					/>
-					<span className="menu-avatar-name">
-						{props.user.firstName} {props.user.lastName}
-					</span>
-				</span>
-			</div>
-			<i
-				className="ionicons icon ion-ios-menu"
-				onClick={() => setMenuActive(!menuActive)}
-			/>
-		</nav>
-	);
+  const [menuActive, setMenuActive] = useState(false);
+  return (
+    <nav className="site-nav">
+      <span className="menu-title">My Blog</span>
+      <div className={`menu-content-container ${menuActive && 'active'}`}>
+        <ul>
+          {navLinks.map((link, index) => (
+            <li key={index}>
+              <NavLink
+                onClick={() => setMenuActive(!menuActive)}
+                exact
+                activeClassName="navsbar"
+                to={link.path}
+              >
+                {link.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+        <span className="menu-avatar-container">
+          <Avatar
+            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            size="38"
+          />
+          <span className="menu-avatar-name">
+            {props.user.firstName} {props.user.lastName}
+          </span>
+        </span>
+      </div>
+      <i
+        className="ionicons icon ion-ios-menu"
+        onClick={() => setMenuActive(!menuActive)}
+      />
+    </nav>
+  );
 }
